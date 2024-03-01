12ebf8d00ca28e3eab4328cead975bdd72f375d0
Sergio Betancor <serbetancor@gmail.com>
2024-03-01 22:26:29 +0000

    Category analysis upgrade

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -7,10 +7,11 @@ def calculate_average(numbers):
     average = total / len(numbers)
     return average
 
-with open('../data/products.json', 'r') as jsonFile:
+with open('../data/productsStradivarius.json', 'r') as jsonFile:
     datos = json.load(jsonFile)
 
 prices = []
+categoryPrices = []
 todo = []
 sections = []
 for category in datos['categories']:
@@ -24,16 +25,18 @@ for category in datos['categories']:
     else:
         for section in category['section']:
             for items in section['products']:
+                categoryPrices.append(items['price'])
                 prices.append(items['price'])
 
             sections.append({ "name": section['name'], "number of items": section['quantity'], "average price": round(calculate_average(prices),2) })
             prices = []
 
-        todo.append({ "name": category['name'], "number of items": category['quantity'], "sections": sections })
+        todo.append({ "name": category['name'], "number of items": category['quantity'], "average price": round(calculate_average(categoryPrices),2), "sections": sections })
+        categoryPrices = []
         sections = []
 
 
-with open('./data/analysisStradivarius.json', 'w', encoding='utf-8') as output_file:
+with open('../data/analysisStradivarius.json', 'w', encoding='utf-8') as output_file:
     jsonData = { "categories": todo }
     json.dump(jsonData, output_file, indent=4)
 
